405a71f1ce27ac8415c21fa85f2c25db57f9527b
sm-animait <RockyRous303@gmail.com>
2024-06-13 18:22:55 +0300

    update

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser, LoyaltyTransaction, Referal, Promotion, News
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(LoyaltyTransaction)
+admin.site.register(Referal)
+admin.site.register(Promotion)
+admin.site.register(News)
