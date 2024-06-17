9e1b5a27f414e88d7b24b3601b0fbd8ec907834e
Elena <fedotova2499@mail.ru>
2024-06-17 22:27:49 +0300

    updateFront

--- templates/slider.js
+++ templates/slider.js
@@ -1,12 +1,10 @@
 const slides = document.querySelectorAll('.advertising__container');
-//const dots = document.querySelectorAll('.dot');
 const intervalSlider = 5000;
 let slideIndex = 0;
 
 function sliderAuto() {
     setInterval(function() {
         slides[slideIndex].classList.remove('advertising_active');
-        //dots[slideIndex].classList.remove('dot_active');
     
         slideIndex++;
 
@@ -15,10 +13,7 @@ function sliderAuto() {
         }
     
         slides[slideIndex].classList.add('advertising_active');
-        //dots[slideIndex].classList.add('dot_active');
     }, intervalSlider);
-        
-    showSlide(slideIndex);
 } 
 
 sliderAuto();  
