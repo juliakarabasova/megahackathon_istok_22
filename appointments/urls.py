bfe2a2c85724aa1f250f304c7f5cd42fb1f50e45
AlexKozyrev <JunkoEnoshimaEvil@yandex.ru>
2024-06-21 20:01:29 +0300

    Чистота и красотв
    -иконка сайта
    -встречи корректно записываются в бд

--- appointments/urls.py
+++ appointments/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.appointment_list, name='appointment_list'),
     path('create/', views.appointment_create, name='appointment_create'),
     path('contact/', views.contact, name='contact'),
+    path('save-appointment/', views.save_appointment, name='save_appointment'),
 ]
