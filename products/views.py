6a893317054fd9981c5805b7f265d5e1e81d22e9
sm-animait <RockyRous303@gmail.com>
2024-06-22 16:59:29 +0300

    add first part

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.core.cache import cache
 
 from products.models import Product
 
@@ -23,3 +24,26 @@ class IndexPageView(ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.order_by('-id')
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'type_of_furniture.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['debug'] = 'This is a debug message'
+        context['product_debug'] = self.object  # Добавьте это для отладки
+
+        return context
+
+    # def get_object(self, *args, **kwargs):  # переопределяем метод получения объекта
+    #     obj = cache.get(f'product-{self.kwargs["pk"]}', None)
+    #
+    #     # если объекта нет в кэше, то получаем его и записываем в кэш
+    #     if not obj:
+    #         obj = super().get_object(queryset=self.queryset)
+    #         cache.set(f'product-{self.kwargs["pk"]}', obj)
+    #
+    #     return obj
